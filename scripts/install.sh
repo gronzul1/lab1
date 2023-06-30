b66ef8ed8bacf49b5d6e349636b5bd0165bf054b
Giuseppe Ronzulli <Giuseppe.Ronzulli@vodafone.com>
2023-06-30 19:59:55 +0200

    add home

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
-sudo su
-yum update -y
-yum install httpd php php-mysqli -y
-cd /var/www/html
-echo "<html><h1>Welcome to Autoscale Instance</h1><div><h1>Hostname $(hostname -f) </h1></div> </html>" > /var/www/html/index.html
-service httpd start
-chkconfig httpd on
+#sudo su
+sudo yum update -y
+sudo yum install httpd php php-mysqli git -y
+
+sudo service httpd start
+sudo chkconfig httpd on
+
+git clone https://github.com/gronzul1/lab1.git
+
 sudo usermod -a -G apache ec2-user
 sudo chown  ec2-user:apache /var/www -R
 # sudo chmod 2775 /var/www
 find /var/www -type d -exec sudo chmod 2775 {} \;
 find /var/www -type f -exec sudo chmod 0664 {} \;
+
+mkdir /var/www/inc
+cp lab1/inc/dbinfo.inc /var/www/inc/
+cp lab1/index.html /var/www/html/
+cp lab1/index.php /var/www/html/
